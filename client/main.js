3880a4b42bcd147cd21e7d7c325127789a478f6d
dannyvassallo <danielvassallo87@gmail.com>
2016-10-13 13:45:38 -0400

    reactive map simple setup following http://meteorcapture.com/how-to-create-a-reactive-google-map/

--- client/main.js
+++ client/main.js
@@ -3,20 +3,65 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.map.onCreated(function() {
+  GoogleMaps.ready('map', function(map) {
+    google.maps.event.addListener(map.instance, 'click', function(event) {
+      Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
+    });
+
+    var markers = {};
+
+    Markers.find().observe({
+      added: function(document) {
+        // Create a marker for this document
+        var marker = new google.maps.Marker({
+          draggable: true,
+          animation: google.maps.Animation.DROP,
+          position: new google.maps.LatLng(document.lat, document.lng),
+          map: map.instance,
+          // We store the document _id on the marker in order
+          // to update the document within the 'dragend' event below.
+          id: document._id
+        });
+
+        // This listener lets us drag markers on the map and update their corresponding document.
+        google.maps.event.addListener(marker, 'dragend', function(event) {
+          Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
+        });
+
+        // Store this marker instance within the markers object.
+        markers[document._id] = marker;
+      },
+      changed: function(newDocument, oldDocument) {
+        markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
+      },
+      removed: function(oldDocument) {
+        // Remove the marker from the map
+        markers[oldDocument._id].setMap(null);
+
+        // Clear the event listener
+        google.maps.event.clearInstanceListeners(
+          markers[oldDocument._id]);
+
+        // Remove the reference to this marker instance
+        delete markers[oldDocument._id];
+      }
+    });
+
+  });
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Meteor.startup(function() {
+  GoogleMaps.load();
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.map.helpers({
+  mapOptions: function() {
+    if (GoogleMaps.loaded()) {
+      return {
+        center: new google.maps.LatLng(40.365092, -74.167465),
+        zoom: 8
+      };
+    }
+  }
 });
